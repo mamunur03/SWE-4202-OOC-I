03ec4146f38ae79cdd31ee0524b406f386ef26ac
mamunur03 <95133485+mamunur03@users.noreply.github.com>
2021-12-11 22:58:48 +0600

    Add files via upload

---
+++ Lab3/CRS Dependency/CRS Dependency/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Rental_dependency
+{
+    public class Car
+    {
+        public string name;
+        public string model;
+        public int number;
+
+        public Car(string name, int number, string model)
+        {
+            this.name = name;
+            this.number = number;
+            this.model = model;
+        }
+
+        public void rented()
+        {
+            this.number--;
+        }
+    }
+}
---
+++ Lab3/CRS Dependency/CRS Dependency/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Rental_dependency
+{
+    public class User
+    {
+        public string name;
+        public int id;
+        public string address;
+        public string destination;
+        public int rentedNum = 0;
+        public User(string name, int id, string address, string destination)
+        {
+            this.name = name;
+            this.id = id;
+            this.address = address;
+            this.destination = destination;
+
+        }
+        public void rentCar()
+        {
+            rentedNum++;
+        }
+
+    }
+}
+
